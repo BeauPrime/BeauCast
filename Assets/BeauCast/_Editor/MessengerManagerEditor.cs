9cbbfe44132e36c123bebad168710a6d2ca6f1bb
abeauchesne <abeauchesne@filamentgames.com>
2020-01-13 16:32:30 -0600

    * Updated name and copyright date

--- Assets/BeauCast/_Editor/MessengerManagerEditor.cs
+++ Assets/BeauCast/_Editor/MessengerManagerEditor.cs
@@ -1,6 +1,6 @@
 ﻿/*
- * Copyright (C) 2017-2019. Filament Games, LLC. All rights reserved.
- * Author:  Alex Beauchesne
+ * Copyright (C) 2017-2020. Filament Games, LLC. All rights reserved.
+ * Author:  Autumn Beauchesne
  * Date:    17 Jan 2017
  * 
  * File:    MessengerManagerEditor.cs
